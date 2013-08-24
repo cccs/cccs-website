714d02631f025eb99ee4b30f19aecf855e92d9e6
Stefan Schlott <stefan@ploing.de>
2013-08-24 11:15:46 +0200

    Latest articles on start page

--- lib/articlehelpers.rb
+++ lib/articlehelpers.rb
@@ -18,3 +18,14 @@ def pathname_of_article(item)
   slug = idparts[2].sub( %r{^[0-9]*-}, "" )
   time.strftime('%Y-%m-%d') + '-' + slug
 end
+
+
+def latest_articles(max=nil)
+  @cache_latest_art ||= @site.items.select do |p|
+    p.attributes[:kind] == 'article'
+  end.sort do |a, b|
+    a.attributes[:created_at] <=> b.attributes[:created_at]
+  end.reverse
+  @cache_latest_art[0..(max ? max-1 : @cache_latest_art.length-1)]
+end
+
