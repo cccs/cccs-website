d6eb173ca91440976d12e8b3f75e4bb6ea86d32c
Stefan Schlott <stefan@ploing.de>
2013-08-25 13:11:37 +0200

    Clarify title

--- lib/eventhelpers.rb
+++ lib/eventhelpers.rb
@@ -1,5 +1,5 @@
 def generate_event_pages()
   articles = items.select { |i| i[:kind] == 'event' }
-  generate_yearly_archive(articles, :startdate, '/events', 'Veranstaltungen', 'event_archive')
+  generate_yearly_archive(articles, :startdate, '/events', 'Veranstaltungen des CCC Stuttgart', 'event_archive')
 end
 
