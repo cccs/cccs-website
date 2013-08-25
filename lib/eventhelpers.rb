5dad6d53a179bdc30d62657580dbf04ce921254c
Stefan Schlott <stefan@ploing.de>
2013-08-25 13:43:45 +0200

    Correct selection

--- lib/eventhelpers.rb
+++ lib/eventhelpers.rb
@@ -1,5 +1,9 @@
+def get_events()
+  items.select { |i| (i[:kind]=='event') && i.identifier.start_with?('/events') }
+end
+
 def generate_event_pages()
-  articles = items.select { |i| i[:kind] == 'event' }
+  articles = get_events()
   generate_yearly_archive(articles, :startdate, '/events', 'Veranstaltungen des CCC Stuttgart', 'event_archive')
 end
 
