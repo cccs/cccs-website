4bc6f6c6af17c1c39605bcbb2260bc65b77dca1e
Stefan Schlott <stefan@ploing.de>
2016-11-30 21:45:01 +0100

    Show placeholder talks in "next talk" box

--- lib/eventhelpers.rb
+++ lib/eventhelpers.rb
@@ -1,5 +1,5 @@
-def get_events()
-  items.select { |i| (i[:kind]=='event') && i.identifier.start_with?('/events') }
+def get_events(includePlaceholders = false)
+  items.select { |i| (i[:kind]=='event') && (i.identifier.start_with?('/events') || (includePlaceholders && i.identifier.start_with?('/_data/platzhalter'))) }
 end
 
 def generate_event_pages()
