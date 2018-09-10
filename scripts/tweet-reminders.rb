b9aa007935d9c21b87c5ae11fbd3f7034b233317
Stefan Schlott <stefan@ploing.de>
2018-09-10 10:19:42 +0200

    Optional length limit as command line parameter

--- scripts/tweet-reminders.rb
+++ scripts/tweet-reminders.rb
@@ -8,8 +8,11 @@ require 'ri_cal'
 days=ARGV[0].to_i
 infotext=ARGV[1]
 
-outputlen=140
-urllen=24
+if (ARGV.length>2)
+  outputlen=ARGV[2].to_i
+else
+  outputlen=140
+end
 
 RiCal.parse($stdin).each do |calendar|
   calendar.events.each do |event|
@@ -17,6 +20,7 @@ RiCal.parse($stdin).each do |calendar|
     if (start-Date.today())==days
       output = "#{infotext} #{event.summary}".byteslice(0,outputlen)
       if event.url and event.url.length>0
+        urllen = event.url.length
         if output.length + urllen + 1 > outputlen
           output = "#{output.byteslice(0,outputlen - urllen - 1)} #{event.url}"
         else
