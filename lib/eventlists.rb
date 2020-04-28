6dceab8eea478f9020a24864989e85406cec03b9
Stefan Schlott <stefan@ploing.de>
2020-04-28 10:40:55 +0200

    Check for empty event lists

--- lib/eventlists.rb
+++ lib/eventlists.rb
@@ -1,13 +1,15 @@
 def expand_event_list(itemId, defaultTitle = nil, skipIfDatePresent = false)
-  @items[itemId][:events].each_with_index do |event,n|
-    if (!skipIfDatePresent || !@items.any?{ |i| conv_tz(i[:startdate])==conv_tz(event[:startdate]) })
-      metadata = {
-        :title => defaultTitle,
-        :public => true
-      }
-      metadata.merge!(event)
-      metadata[:kind]='event'
-      @items << Nanoc::Item.new("<%= render 'event_body' %>", metadata, "#{itemId}#{n}/")
+  if @items[itemId][:events].instance_of? Nanoc::ItemArray
+    @items[itemId][:events].each_with_index do |event,n|
+      if (!skipIfDatePresent || !@items.any?{ |i| conv_tz(i[:startdate])==conv_tz(event[:startdate]) })
+        metadata = {
+          :title => defaultTitle,
+          :public => true
+        }
+        metadata.merge!(event)
+        metadata[:kind]='event'
+        @items << Nanoc::Item.new("<%= render 'event_body' %>", metadata, "#{itemId}#{n}/")
+      end
     end
   end
 end
