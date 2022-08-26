ab2c3554521b48e1c88936a55a001636d5a15523
Stefan Schlott <stefan@ploing.de>
2022-08-26 20:47:06 +0200

    Missing reminder script in container

--- scripts/send-reminder-tweets.sh
+++ scripts/send-reminder-tweets.sh
@@ -6,27 +6,32 @@ if [[ -f "/usr/local/bundle/bin/t" ]] ; then
 else
   T="bundle exec t"
 fi
+if [[ -f "/usr/local/bin/tweet-reminders.rb" ]] ; then
+  REMINDERS=/usr/local/bin/tweet-reminders.rb
+else
+  REMINDERS=scripts/tweet-reminders.rb
+fi
 TOOT=${TOOT:-/usr/local/bin/toot}
 
 cd `dirname $0`/..
 
-cat "$WEBSITE_DIR/cccs.ical" | scripts/tweet-reminders.rb 0 "Heute:" | while read LINE ; do
+cat "$WEBSITE_DIR/cccs.ical" | "$REMINDERS" 0 "Heute:" | while read LINE ; do
 	$T update "$LINE"
 done
-cat "$WEBSITE_DIR/cccs.ical" | scripts/tweet-reminders.rb 1 "Morgen:" | while read LINE ; do
+cat "$WEBSITE_DIR/cccs.ical" | "$REMINDERS" 1 "Morgen:" | while read LINE ; do
 	$T update "$LINE"
 done
-cat "$WEBSITE_DIR/cccs.ical" | scripts/tweet-reminders.rb 7 "In einer Woche:" | while read LINE ; do
+cat "$WEBSITE_DIR/cccs.ical" | "$REMINDERS" 7 "In einer Woche:" | while read LINE ; do
 	$T update "$LINE"
 done
 
-cat "$WEBSITE_DIR/cccs.ical" | scripts/tweet-reminders.rb 0 "Heute:" 500 | while read LINE ; do
+cat "$WEBSITE_DIR/cccs.ical" | "$REMINDERS" 0 "Heute:" 500 | while read LINE ; do
 	$TOOT "$LINE"
 done
-cat "$WEBSITE_DIR/cccs.ical" | scripts/tweet-reminders.rb 1 "Morgen:" 500 | while read LINE ; do
+cat "$WEBSITE_DIR/cccs.ical" | "$REMINDERS" 1 "Morgen:" 500 | while read LINE ; do
 	$TOOT "$LINE"
 done
-cat "$WEBSITE_DIR/cccs.ical" | scripts/tweet-reminders.rb 7 "In einer Woche:" 500 | while read LINE ; do
+cat "$WEBSITE_DIR/cccs.ical" | "$REMINDERS" 7 "In einer Woche:" 500 | while read LINE ; do
 	$TOOT "$LINE"
 done
 
